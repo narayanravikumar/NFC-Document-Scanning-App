162d5f65d22026e2fe58f63d1701d5c24e562381
Ramkumar M <Ramk04667@gmail.com>
2021-06-30 02:29:30 +0530

    added firebase connections .

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
@@ -9,31 +10,85 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView registertv;
     Button loginbtn, needhelpbtn;
+    EditText email,password;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
         getSupportActionBar().hide(); // hide the title bar
+
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-
+        email=(EditText) findViewById(R.id.etEmailIDatatLogin);
+        password=(EditText) findViewById(R.id.etPasswordatLogin);
         registertv = (TextView) findViewById(R.id.tvRegister);
         loginbtn = (Button) findViewById(R.id.btnLogin);
         needhelpbtn = (Button) findViewById(R.id.btnNeedHelp);
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("nfc-protector-firebase-adminsdk-h2y9l-803bb60a60.json");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setProjectId("nfc-protector")
+                .setApplicationId("1:35608449255:android:82208789b230eedb831242")
+                .setApiKey("AIzaSyB3QCNmiAilFr5jbz6qIq2KCdmp3kSSqqs")
+                .build();
+
+        FirebaseApp.initializeApp(this,options,"active");
+
+        auth= FirebaseAuth.getInstance(FirebaseApp.getInstance("active"));
+
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ScanActivity.class);
-                startActivity(intent);
+                if(email != null && password != null){
+                    String emailtext=email.getText().toString();
+                    String passwordtext = password.getText().toString();
+
+                    //authenticate the user
+                    auth.signInWithEmailAndPassword(emailtext,passwordtext).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(getApplicationContext(),"Login Successful", Toast.LENGTH_LONG).show();
+                                Intent intent= new Intent(MainActivity.this, ScanActivity.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(getApplicationContext(),"Login Error", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }else{
+                    Toast.makeText(getApplicationContext(),"Email and Password Should Not Be NULL",Toast.LENGTH_LONG).show();
+                }
             }
         });
 
@@ -43,14 +98,11 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"Enter credentials", Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
-        registertv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
-                startActivity(intent);
-            }
-        });
+    public void register(View view) {
+        Intent intent = new Intent(getApplicationContext(), RegistrationActivity.class);
+        startActivity(intent);
 
     }
 }
--- app/src/main/java/com/example/myapplication/RegistrationActivity.java
+++ app/src/main/java/com/example/myapplication/RegistrationActivity.java
@@ -3,12 +3,43 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    /*Button register=(Button) findViewById(R.id.register);
+    EditText username,usn, email,pass,repass;
+    FirebaseAuth fauth;
+    */
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+       /* username=(EditText) findViewById(R.id.etUserName);
+        usn=(EditText) findViewById(R.id.etUSNatreg);
+        email=(EditText) findViewById(R.id.etEmailIDatreg);
+        fauth=FirebaseAuth.getInstance();
+        pass=(EditText) findViewById(R.id.atPasswordatreg);
+        repass=(EditText) findViewById(R.id.etRepasswordatreg);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(pass==repass && username != null && usn != null && email != null && pass != null){
+
+                }else{
+                    Toast.makeText(getApplicationContext(),"Password doesn't Match With Retyped Password ",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });*/
     }
+
 }
