b27bc8a5f87c11f390d2d817dcec65e8e6c60145
Ramkumar M <Ramk04667@gmail.com>
2021-08-15 00:08:08 +0530

    Added Menu

---
+++ app/src/main/java/com/example/myapplication/Menu.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button write = (Button) findViewById(R.id.btn_write);
+        Button  read = (Button) findViewById(R.id.btn_read);
+
+        write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Menu.this, Write.class);
+                startActivity(intent);
+            }
+        });
+
+        read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Menu.this, ScanActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+
+}
